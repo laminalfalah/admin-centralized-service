79cfe345aa059abbb70076b85f8eaa9ba52c4de6
Laminal Falah <laminalfalah08@gmail.com>
2023-11-23 17:53:11 +0700

    Remove enable web flux

--- src/main/java/io/github/laminalfalah/admin/centralized/AdminCentralizedApplication.java
+++ src/main/java/io/github/laminalfalah/admin/centralized/AdminCentralizedApplication.java
@@ -4,9 +4,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
-@EnableWebFlux
 @EnableAdminServer
 @EnableDiscoveryClient
 @SpringBootApplication
